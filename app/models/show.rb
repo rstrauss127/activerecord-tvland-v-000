aab2071836b3c71d42470bf1bf7bd93e794b62f1
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-24 19:58:11 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
-  attr_accessor :genre
+  attr_accessor :genre, :name
   def build_network(call_letters)
   end
 end
