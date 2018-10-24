fd05b7c7e5d5841478d6d8ccac68a1c61d5c9d7e
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-24 19:37:41 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
-  attr_accessor :name, :day, :season
+  
   def build_network(call_letters)
   end
 end
